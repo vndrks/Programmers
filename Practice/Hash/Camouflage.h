aa8263576a561c60f0bbcc7cf26d8c17d0c20716
jc.kang <vndrks@naver.com>
2019-11-14 18:15:47 +0900

    [Title] Add Camouflage.h
    [Contents]
    -

---
+++ Practice/Hash/Camouflage.h
@@ -0,0 +1,18 @@
+#include <string>
+#include <vector>
+#include <unordered_map>
+#include <unordered_set>
+
+using namespace std;
+
+class Camouflage
+{
+public:
+	int solution(vector<vector<string>> clothes);
+	
+	int solution2(vector<vector<string>> clothes);
+
+private:
+
+
+};
